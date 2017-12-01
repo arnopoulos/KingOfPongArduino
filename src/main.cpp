61bc52b8c9d33479350fcd284bee4c18008c12f6
Jace Conflenti <jace.conflenti@workday.com>
2017-11-30 21:50:55 -0700

    single button working besides double click issue

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,66 @@
 #include <Arduino.h>
+#include <SimpleTimer.h>
+
+#define buttonPin D0
+
+boolean holding = false;
+boolean isLongPress = false;
+boolean doubleClickPossible = false;
+SimpleTimer timer;
+int longPressTimerToken = -1;
+int doubleClickTimerToken = -1;
+
+void clickEvent() {
+  Serial.println("Single Click");
+}
+
+void doubleClickEvent() {
+  Serial.println("Double Click");
+}
+
+void holdEvent() {
+  Serial.println("Hold");
+}
 
 void setup() {
-    // put your setup code here, to run once:
+  Serial.begin(9600);
+  // Set button input pin
+  pinMode(buttonPin, INPUT);
+
+  timer = SimpleTimer();
+  timer.setInterval(50, []() {
+    boolean currentValue = digitalRead(buttonPin);
+    if (currentValue && !holding && !doubleClickPossible) {
+      longPressTimerToken = timer.setTimeout(750, []() {
+        isLongPress = true;
+        holdEvent();
+      });
+    }
+    if (currentValue && !holding && doubleClickPossible) {
+      Serial.println("Double click timer will invalidate");
+      timer.deleteTimer(doubleClickTimerToken);
+      doubleClickTimerToken = -1;
+      doubleClickEvent();
+      doubleClickPossible = false;
+    }
+    if (!currentValue && holding && !isLongPress) {
+      if (longPressTimerToken != -1) {
+        timer.deleteTimer(longPressTimerToken);
+        longPressTimerToken = -1;
+      }
+      doubleClickTimerToken = timer.setTimeout(250, []() {
+        doubleClickPossible = false;
+        clickEvent();
+      });
+      doubleClickPossible = true;
+    }
+    if (!currentValue && holding && isLongPress) {
+      isLongPress = false;
+    }
+    holding = currentValue;
+  });
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+  timer.run();
 }
